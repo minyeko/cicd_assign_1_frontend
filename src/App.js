2ad6572bc3416fd452965e1db0d53fde3821a25c
minyeko <minyeko@outlook.com>
2024-07-11 11:15:05 +1200

    add missing files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './components/AuthContext';
+import TopBar from './components/TopBar';
+import Login from './components/Login';
+import Register from './components/Register';
+import MainForm from './components/MainForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <AuthProvider>
+                <TopBar />
+                <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/main" element={<MainForm />} />
+                    {/* other routes */}
+                </Routes>
+        </AuthProvider>
+    );
+};
 
 export default App;
